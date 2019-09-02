44d400500ef948c2898f55fe4259a6c32a80c67e
Ally Morson <ally.morson@gmail.com>
2019-09-02 23:20:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,23 @@
 def addition(num1, num2)
-
+puts num1 + num2
 end
+addition(1, 2)
 
 def subtraction(num1, num2)
-
+puts num1 - num2
 end
+subtraction(1, 2)
 
 def division(num1, num2)
-
+puts num1 / num2
 end
 
 def multiplication(num1, num2)
-
+puts num1 * num2
 end
 
 def modulo(num1, num2)
-
+puts num2 + num1
 end
 
 def square_root(num)
