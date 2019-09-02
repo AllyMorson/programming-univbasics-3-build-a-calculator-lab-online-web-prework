b43593677821a597ea8077a147ca1ec08ad74dbf
Ally Morson <ally.morson@gmail.com>
2019-09-02 23:28:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,10 @@
 def addition(num1, num2)
-  num1 = 1
-  num2 = 2 
-puts num1 + num2
+  puts (num1 + num2)
 end
 
 def subtraction(num1, num2)
 puts num1 - num2
 end
-subtraction(1, 2)
 
 def division(num1, num2)
 puts num1 / num2
